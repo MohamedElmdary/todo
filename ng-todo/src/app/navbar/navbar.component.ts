0881c854f8f936c6a7f042d0a74df65706a5288e
MohamedElmdary <engm5081@gmail.com>
2019-02-16 14:54:41 +0200

    added auth to navbar

--- ng-todo/src/app/navbar/navbar.component.ts
+++ ng-todo/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../auth/User.interfaces';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isAuth: User = null;
+
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+    this.auth.isAuth.subscribe(
+      (data: User) => this.isAuth = data
+    );
   }
 
 }
